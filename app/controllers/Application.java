d5a688d03a17a219cfb9b4b7b698f872df7b10a6
Kelsie Belan <kelsie70@gmail.com>
2017-11-28 23:39:48 -0500

    added login page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,9 +6,14 @@ import play.mvc.*;
 public class Application extends Controller {
 
     @Inject views.html.index indexTemplate;
+    @Inject views.html.login loginTemplate;
 
     public Result index() {
         return ok(indexTemplate.render());
+    }
+    
+    public Result login() {
+        return ok(loginTemplate.render());
     }
 
 }
