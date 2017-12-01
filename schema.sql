634f3ac9fff98f9681d50838b7e20b081558e350
Kyle Millar <kymilla608@gmail.com>
2017-12-01 14:01:36 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE DatabaseFinal /*!40100 DEFAULT CHARACTER SET latin1 */;
 
 CREATE TABLE `Profile` (
-  `user_id` integer NOT NULL,
+  `user_id` integer NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `first_name` varchar(255) DEFAULT NULL,
@@ -12,7 +12,8 @@ CREATE TABLE `Profile` (
   `zip_code` integer DEFAULT NULL,
   `email` varchar(255) NOT NULL,
   `balance` varchar(255) NOT NULL,
-  PRIMARY KEY (`user_id`)
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `Category` (
@@ -22,7 +23,7 @@ CREATE TABLE `Category` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `Item` (
-  `item_id` integer NOT NULL,
+  `item_id` integer NOT NULL AUTO_INCREMENT,
   `item_name` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   `price` double DEFAULT NULL,
@@ -32,14 +33,14 @@ CREATE TABLE `Item` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `Buyer` (
-  `buyer_id` integer NOT NULL,
+  `buyer_id` integer NOT NULL AUTO_INCREMENT,
   `user_id` integer NOT NULL,
   PRIMARY KEY (`buyer_id`),
   FOREIGN KEY (`user_id`) REFERENCES `Profile`(`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `Seller` (
-  `seller_id` integer NOT NULL,
+  `seller_id` integer NOT NULL AUTO_INCREMENT,
   `user_id` integer NOT NULL,
   PRIMARY KEY (`seller_id`),
   FOREIGN KEY (`user_id`) REFERENCES `Profile`(`user_id`)
@@ -49,17 +50,17 @@ CREATE TABLE `Inventory` (
   `item_id` integer NOT NULL,
   `seller_id` integer NOT NULL,
   `number_in_stock` integer DEFAULT NULL,
-  `itemStatus` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`item_id`,`seller_id`),
   FOREIGN KEY (`item_id`) REFERENCES `Item`(`item_id`),
   FOREIGN KEY (`seller_id`) REFERENCES `Seller`(`seller_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `Purchase` (
-  `purchase_id` integer NOT NULL,
+  `purchase_id` integer NOT NULL AUTO_INCREMENT,
   `buyer_id` integer NOT NULL,
   `seller_id` integer NOT NULL,
   `item_id` integer DEFAULT NULL,
+  `time` integer NOT NULL,
   PRIMARY KEY (`purchase_id`),
   FOREIGN KEY (`item_id`) REFERENCES `Item`(`item_id`),
   FOREIGN KEY (`seller_id`) REFERENCES `Seller`(`seller_id`),
